f840568fcfcc422851ed78780a42436c231c77f6
T.Khairalla <66671851+taxido3@users.noreply.github.com>
2021-08-01 14:37:42 +0200

    Add files via upload

---
+++ groupLogLoader.php
@@ -0,0 +1,44 @@
+<?php
+
+$groupConversationData = file_get_contents('JSONdata/TellerMitte Design.json', 'r');
+$groupConversationDataArray = json_decode($groupConversationData, true);
+
+echo '<div>
+		<a href="marvinLogLoader.php"><button type="button">Marvin</button></a>&emsp;
+		<a href="paulLogLoader.php"><button type="button">Paul</button></a>&emsp;
+		<a href="vanLogLoader.php"><button type="button">Van</button></a><br>
+	    <a href="svenLogLoader.php"><button type="button">Sven</button></a>&emsp;
+		<a href="manuelLogLoader.php"><button type="button">Manuel</button></a>&emsp;
+		<a href="groupLogLoader.php"><button type="button">Tellermitte Group</button></a>
+	</div>';
+
+$output = '<div style="display: table; width: 720px">';
+
+$output .= '<table style="border:1px solid; font-size: 18pt;>';
+
+for($i = count($groupConversationDataArray)-1; $i >= 0 ; $i--) {
+	foreach($groupConversationDataArray[$i] as $key => $value) {
+
+		switch ($key){
+			case 'displayName':
+				if (!$value) {
+					$output .= '<tr><td style="border:1px solid; font-weight: bold"> Tarik </td></tr>';
+				}
+				else {
+					$output .= '<tr><td style="border:1px solid; font-weight: bold">'.$value.'</td></tr>';
+				}
+				break;
+			case 'originalarrivaltime':
+				$output .= '<tr><td style="border:1px solid; color: red">'.$value.'</td></tr>';
+				break;
+			case 'content':
+				$output .= '<tr><td style="border:1px solid; background-color: rgba(5,5,5,0.3); padding-left: 35px;">'.$value.'</td></tr>';
+				break;
+			default: 
+				break;
+		}		
+	}
+}
+$output .= '</table></div>';
+echo $output;
+?>
